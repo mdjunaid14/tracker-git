8e59fe5a525b941a695672afa4b890b714295d27
mdjunaid14 <mdjunaid14@outlook.com>
2022-02-10 19:29:59 +0530

    first commit for react-heroku

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import About from './components/About'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  // Get Tasks 
+  useEffect(() => {
+    const getTasks = () => {
+
+      const tasksFromServer = [
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true
+    },
+    {
+      text: "Writing Code",
+      day: "Friday",
+      reminder: false,
+      id: 3
+    },
+    {
+      text: "Building Applications",
+      day: "Thursday",
+      reminder: true,
+      id: 4
+    }]
+
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+
+  // Add Task
+  const addTask = (task) => {
+
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
+  return (
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
